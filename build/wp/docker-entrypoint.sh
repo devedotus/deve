5eab6182ab9f62c083d55ac6db34848eb772c190
Thomas Stachl <thomas@stachl.me>
2018-03-07 19:52:51 -0800

    run wp with dev verison of deve cli

--- build/wp/docker-entrypoint.sh
+++ build/wp/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 
-echo "docker-entrypoint.sh"
+# For development purposes
+export PATH="/var/cli/vendor/bin:$PATH"
 
 # if we don't have the default let's set it up
 if [ ! -d "/var/www/$DEVE_DOMAIN" ]; then
@@ -9,18 +10,7 @@ if [ ! -d "/var/www/$DEVE_DOMAIN" ]; then
         sleep 1
     done
 
-    PASSWDDB=$(openssl rand -base64 24)
-    mysql -h"mysql" -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${DEVE_DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -h"mysql" -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '${DEVE_USER}'@'%' IDENTIFIED BY '${PASSWDDB}';"
-    mysql -h"mysql" -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${DEVE_DATABASE}.* TO '${DEVE_USER}'@'%';"
-    mysql -h"mysql" -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-
-    mkdir -p "/var/www/$DEVE_DOMAIN" && cd $_
-    wp core download
-    wp core config --dbname=$DEVE_DOMAIN --dbuser=$DEVE_DOMAIN --dbpass=$PASSWDDB
-    wp db create
-    wp core install --url=$DEVE_DOMAIN --title="$DEVE_TITLE" --admin_user=$DEVE_USER --admin_password=$DEVE_PASSWORD --admin_email=$DEVE_EMAIL
-    # set -- wp "deve site create $DEVE_DOMAIN"
+    set -- wp deve setup
 fi
 
 # first arg is `-f` or `--some-option`

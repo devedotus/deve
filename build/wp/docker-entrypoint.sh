eac5257f97d3191976c934af235a9e16f3a76d38
Thomas Stachl <thomas@stachl.me>
2018-03-08 19:24:12 -0800

    wait for mysql to run wp
    setup doesn't work from entrypoint as the dir ownership is still with root at that point

--- build/wp/docker-entrypoint.sh
+++ build/wp/docker-entrypoint.sh
@@ -4,14 +4,10 @@ set -euo pipefail
 # For development purposes
 export PATH="/var/cli/vendor/bin:$PATH"
 
-# if we don't have the default let's set it up
-if [ ! -d "/var/www/$DEVE_DOMAIN" ]; then
-    while ! mysqladmin ping -h"mysql" --silent; do
-        sleep 1
-    done
-
-    set -- wp deve setup
-fi
+# check if we have mysql running first
+while ! mysqladmin ping -h"mysql" --silent; do
+    sleep 1
+done
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
